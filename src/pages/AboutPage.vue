0ec5c43d4e8976606d6006a28212db34a79fe382
MyMonsterVR <mymonsterdk@gmail.com>
2023-08-08 11:02:49 +0200

    Initial commit

---
+++ src/pages/AboutPage.vue
@@ -0,0 +1,37 @@
+<template>
+  <q-page class="row items-center justify-evenly">
+    <example-component
+      title="Example component"
+      active
+      :todos="todos"
+      :meta="meta"
+    ></example-component>
+  </q-page>
+</template>
+
+<script lang="ts">
+import { Todo, Meta } from 'components/models';
+import ExampleComponent from 'components/ExampleComponent.vue';
+import { defineComponent, ref } from 'vue';
+
+export default defineComponent({
+  name: 'AboutPage',
+  components: { ExampleComponent },
+  setup() {
+    const todos = ref<Todo[]>([
+      {
+        id: 1,
+        content: 'ct1',
+      },
+      {
+        id: 2,
+        content: 'ct2',
+      },
+    ]);
+    const meta = ref<Meta>({
+      totalCount: 1200,
+    });
+    return { todos, meta };
+  },
+});
+</script>
