e7b6db348ada396347d610aedebf6c39ce0bc246
Marcus Carlsen <marcuscarlsen@hotmail.dk>
2023-08-29 10:03:56 +0200

    Tjek om man allerede har en Twilio access token

--- src/pages/JoinRoomPage.vue
+++ src/pages/JoinRoomPage.vue
@@ -96,22 +96,24 @@ export default defineComponent({
     };
 
     const joinRoom = async () => {
-      api
-        .get('/api/joinRoom', {
-          params: {
-            roomName: roomName.value,
-          },
-          headers: {
-            'Content-Type': 'application/json',
-            Authorization: 'Bearer ' + Cookies.get('token'),
-          },
-        })
-        .then((response) => {
-          localStorage.setItem('TwilioToken', response.data.twilioToken);
-        })
-        .catch((error) => {
-          console.log('Error', error.response);
-        });
+      if (!localStorage.getItem('TwilioToken')) {
+        await api
+          .get('/api/joinRoom', {
+            params: {
+              roomName: roomName.value,
+            },
+            headers: {
+              'Content-Type': 'application/json',
+              Authorization: 'Bearer ' + Cookies.get('token'),
+            },
+          })
+          .then((response) => {
+            localStorage.setItem('TwilioToken', response.data.twilioToken);
+          })
+          .catch((error) => {
+            console.log('Error', error.response);
+          });
+      }
 
       // find or create a room with the given roomName
       const room = await connectRoom(
