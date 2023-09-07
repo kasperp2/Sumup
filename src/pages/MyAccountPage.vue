5c23c4ef88a07624e0dd5828f4ef58131a688fbc
MyMonsterVR <mymonsterdk@gmail.com>
2023-09-07 10:31:35 +0200

    Loading spinner, fetching data

--- src/pages/MyAccountPage.vue
+++ src/pages/MyAccountPage.vue
@@ -1,6 +1,17 @@
 <template>
   <q-page>
-    <div class="flex flex-col items-center mt-4">
+    <div v-if="!isLoaded" class="flex flex-col items-center mt-4">
+      <q-circular-progress
+        indeterminate
+        rounded
+        size="100px"
+        color="primary"
+        class="q-ma-md"
+      />
+      <div class="text-h5 mt-2">Loading...</div>
+      <p>{{ isLoaded }}</p>
+    </div>
+    <div v-else class="flex flex-col items-center mt-4">
       <div class="text-h4">My Account</div>
       <div class="text-h5 mt-2">{{ username }}</div>
       <q-input filled v-model="email" type="email" label="Email" class="w-64" />
@@ -46,13 +57,14 @@
 <script lang="ts">
 import { api } from 'src/boot/axios';
 import { defineComponent, ref } from 'vue';
-import { useQuasar } from 'quasar';
+import { Cookies, useQuasar } from 'quasar';
 
 export default defineComponent({
   name: 'MyAccountPage',
   components: {},
   setup() {
     const $q = useQuasar();
+    const isLoaded = ref(false);
 
     const email = ref('Doe@John.com');
     const username = ref('John Doe');
@@ -60,6 +72,25 @@ export default defineComponent({
     const newPassword = ref('');
     const confirmPassword = ref('');
     const expanded = ref(false);
+
+    api
+      .get('/api/fetchInfo', {
+        headers: {
+          Authorization: `Bearer ${Cookies.get('token')}`,
+        },
+      })
+      .then((response) => {
+        if (response.data.msg) {
+          email.value = response.data.msg[0].email;
+          username.value = response.data.msg[0].username;
+          isLoaded.value = true;
+          return;
+        }
+        $q.notify(response.data.message);
+      })
+      .catch((error) => {
+        $q.notify(error.message);
+      });
 
     const updateInformation = () => {
       if (email.value.trim() === '') {
@@ -108,6 +139,7 @@ export default defineComponent({
       confirmPassword,
       expanded,
       updateInformation,
+      isLoaded,
     };
   },
 });
