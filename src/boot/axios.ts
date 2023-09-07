16b9615db7428ee56f0146e56fa46f37bb4709c9
William Andersen <45121289+williloft@users.noreply.github.com>
2023-09-07 08:18:50 +0200

    fixed root

--- src/boot/axios.ts
+++ src/boot/axios.ts
@@ -4,7 +4,7 @@ import { boot } from 'quasar/wrappers';
 import axios from 'axios';
 
 // TODO: use env variables
-const api = axios.create({ baseURL: 'http://localhost:3000' });
+const api = axios.create({ baseURL: 'http://pi.local:3000' });
 
 export default boot(({ app }) => {
   // for use inside Vue files (Options API) through this.$axios and this.$api
